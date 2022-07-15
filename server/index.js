cb2458863f2be4c82eaa1020a93c4099274426aa
Bhavya Verma <vbhavya269@gmail.com>
2022-07-15 23:17:39 +0530

    ✨ Setting up express server

--- server/index.js
+++ server/index.js
@@ -3,3 +3,10 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
