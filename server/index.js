cc0edb14320510bf8f967cbf3fc1c6437aaaa04b
Bhavya Verma <vbhavya269@gmail.com>
2022-07-18 22:13:15 +0530

    ✨ Connecting backend to the DB

--- server/index.js
+++ server/index.js
@@ -9,4 +9,22 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+
+const CONNECTION_URL = "mongodb+srv://Bhavya:Bhavya123@cluster0.jfzox.mongodb.net/?retryWrites=true&w=majority";
+const POST = process.env.PORT || 5000;
+
+// not necessary but might get warnings in the console
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        }
+        )
+    .catch((error) => {
+        console.log(error.message)
+    })
+    })
+
+// To avoid any warnings in the console
+mongoose.set('useFindAndModify', false);
 // https://www.mongodb.com/cloud/atlas
